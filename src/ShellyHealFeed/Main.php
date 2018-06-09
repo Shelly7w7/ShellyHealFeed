25683212773f6bf014b395257182647fd6bdf81b
Shelly7w7 <36070804+Shelly7w7@users.noreply.github.com>
2018-06-09 20:06:16 +0500

    Create Main.php

--- src/ShellyHealFeed/Main.php
+++ src/ShellyHealFeed/Main.php
@@ -1,1 +1,50 @@
+<?php
 
+namespace ShellyHealFeed;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class Main extends PluginBase{
+
+    public function onEnable(){
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
+
+        switch($cmd->getName()){
+
+            case "heal":
+                if($sender instanceof Player){
+			if($sender->hasPermission("shellyheal.command")){
+				$sender->setHealth(20);
+				$sender->addTitle("§cHealed");
+				$sender->sendMessage("§l§5ShellyHeal > §r§cYou Have Been Healed!");
+			}
+			   
+                }
+            break;
+
+            case "feed":
+                if($sender instanceof Player){
+			if($sender->hasPermission("shellyfeed.command")){
+				$sender->setFood(20);
+				$sender->addTitle("§cFeed");
+				$sender->sendMessage("§l§5ShellyHeal > §r§cYou Have Been Fed!");
+			}
+			   
+                }
+            break;
+
+        }
+
+        return true;
+
+    }
+
+}
+		
